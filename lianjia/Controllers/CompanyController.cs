7f06e6f64ead5895b985a55102e9af9494436033
David Zheng <nmdigboy@hotmail.com>
2020-05-06 03:14:49 +0800

    初始化链家项目

---
+++ lianjia/Controllers/CompanyController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace lianjia.Controllers
+{
+    
+    [RoutePrefix("companyinfo")]
+
+    public class CompanyController : ApiController
+    {
+
+        [Route("regCompany")]
+        public IHttpActionResult RegisterCompany()
+        {
+            return Ok();
+
+        }
+
+
+
+    }
+}
